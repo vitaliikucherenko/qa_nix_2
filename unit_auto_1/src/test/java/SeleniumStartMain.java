4b7de8e1402d540b28e09db2b2d30d90ae35544d
vitaliikucherenko <kucherenko.vitalik@gmail.com>
2021-10-19 18:19:58 +0300

    fully implemented version split 10 tests into 10 different test methods added Assertions

--- unit_auto_1/src/test/java/SeleniumStartMain.java
+++ unit_auto_1/src/test/java/SeleniumStartMain.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
